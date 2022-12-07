c2acc5c759662e8cd0e70b82d3c37e0d6cb35ee9
TAPACplay <118310674+TAPACplay@users.noreply.github.com>
2022-12-07 23:22:48 +0200

    Add project files.

---
+++ 1 (2)/1 (2).cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+//математика, фізика, біологія, іноземна мова, географія
+int main() {
+    setlocale(0, "");
+    cout << "бал з математики \n";
+    float a;
+    cin >> a;
+    float b;
+    cout << "бал з Фiзики  \n";
+    cin >> b;
+    float d;
+    float c;
+    float e;
+    float f;
+    cout << "бал з iноземної мови \n";
+    cin >> c;
+    cout << "бал з географiї \n";
+    cin >> d;
+    cout << "бал з бiологiї \n";
+    cin >> e;
+    f = (a + b + c + d + e) / 5;
+    cout << f;
+}
